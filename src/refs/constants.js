4c43dc2289a81b3b50107b0de9d1965ad357319f
Favixx <bogdandom52@gmail.com>
2023-08-01 09:41:28 +0300

    added service functions and constants

---
+++ src/refs/constants.js
@@ -0,0 +1,13 @@
+export const statusFilters = ['Show all', 'Follow', 'Followings'];
+
+export const lsKeys = Object.freeze({
+  users: 'users',
+  filter: 'filter',
+  followings: 'followings',
+});
+
+export const limit = 3;
+
+export const totalItems = 12;
+
+export const baseUrlApi = 'https://64abd8989edb4181202ea92b.mockapi.io/';
