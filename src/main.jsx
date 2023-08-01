29bb6b244d136fea8774307ab20e8ee7fc63a484
Favixx <bogdandom52@gmail.com>
2023-08-01 10:10:24 +0300

    trying to fix SPA deploy

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/followers-task/'>
+  <BrowserRouter basename='/followers-task'>
     <App />
   </BrowserRouter>,
 )
